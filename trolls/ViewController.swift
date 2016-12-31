0aa3b15a2306834bdf1cb260e790338250ef85bf
Farrukh Askari <farrukh.askari01@gmail.com>
2016-12-31 23:53:29 +0500

    Initial Commit WebView setup with replace functionality

--- trolls/ViewController.swift
+++ trolls/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
+    @IBOutlet weak var pageView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+   
+        if let url = URL(string: "http://123movies.is") {
+            do {
+                let contents = try String(contentsOf: url)
+                print(contents)
+                let myString = contents.replacingOccurrences(of: "", with: "").replacingOccurrences(of: "", with: "").replacingOccurrences(of: "", with: "")
+                pageView.loadHTMLString(myString, baseURL: nil)
+            } catch {
+                // contents could not be loaded
+                print("err")
+            }
+        } else {
+            // the URL was bad!
+            print ("bad url")
+        }
+        
+        // Normal Loading
+//        pageView.delegate = self
+//        pageView.loadRequest(URLRequest(url: URL(string: "http://123movies.is")!))
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
